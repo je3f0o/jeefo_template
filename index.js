d3192992b958b89124ebf92be6a3257345c0ac12
je3f0o <je3f0o@gmail.com>
2017-06-03 00:47:21 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+/* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
+* File Name   : index.js
+* Created at  : 2017-04-26
+* Updated at  : 2017-06-03
+* Author      : jeefo
+* Purpose     :
+* Description :
+_._._._._._._._._._._._._._._._._._._._._.*/
+//ignore:start
+"use strict";
+
+/* global */
+/* exported */
+/* exported */
+
+//ignore:end
+
+var _package = require("./package");
+
+module.exports = require(`./dist/${ _package.name }.node.min`);
